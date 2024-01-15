84607464b99133f35ec4383d00d3a928dd6bb90d
pragy-dl <148120781+pragy-dl@users.noreply.github.com>
2024-01-15 22:23:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/pragy-dl/AnnayanXMUsic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
