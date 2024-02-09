34852c23d07b3aa0355ee0c2e72253791230af2a
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-02-09 09:43:03 +0530

    Update repo.py

--- AnnayanX/plugins/bot/repo.py
+++ AnnayanX/plugins/bot/repo.py
@@ -4,19 +4,8 @@ from AnnayanX import app
 from config import BOT_USERNAME
 
 start_txt = """**
-(っ◔◡◔)っ ♥ ✪ ωεℓ¢σмє ƒσя AnnayanX яєρσѕ ✪
- 
- ➲ ᴀʟʟ ʀᴇᴘᴏ ᴇᴀsɪʟʏ ᴅᴇᴘʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴀɴʏ ᴇʀʀᴏʀ ✰
- 
- ➲ ɴᴏ ʜᴇʀᴏᴋᴜ ʙᴀɴ ɪssᴜᴇ ✰
- 
- ➲ ɴᴏ ɪᴅ ʙᴀɴ ɪssᴜᴇ ✰
- 
- ➲ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏs ✰
- 
- ➲ ʀᴜɴ 24x7 ʟᴀɢ ғʀᴇᴇ ᴡɪᴛʜᴏᴜᴛ sᴛᴏᴘ ✰
- 
- ► ɪғ ʏᴏᴜ ғᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛʜᴇɴ sᴇɴᴅ ss
+(っ◔◡◔)っ ♥ ✪ Welcome For AnnayanX Repos
+𝙱𝚑𝚊𝚒 𝚁𝚎𝚙𝚘 𝚔𝚎 𝙻𝚒𝚢𝚎 𝚂𝚑𝚊𝚔𝚊𝚕 𝙳𝚎𝚔𝚑𝚒 𝙷𝚊𝚒 𝙺𝚑𝚞𝚍𝚔𝚒 𝙳𝚊𝚏𝚊 𝚑𝚘𝚓𝚊 @𝙰𝚗𝚗𝚊𝚢𝚊𝚗𝚇
 **"""
 
 
@@ -30,39 +19,12 @@ async def start(_, msg):
           InlineKeyboardButton("𝗔𝗗𝗗 𝗠𝗘", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗛𝗘𝗟𝗣", url="https://t.me/VrindavanNEeko16008"),
+          InlineKeyboardButton("𝗛𝗘𝗟𝗣", url="https://t.me/Vrindavaneeko16008"),
           InlineKeyboardButton("𝗢𝗪𝗡𝗘𝗥", url="https://t.me/AnnayanX"),
           ],
-               [
-                InlineKeyboardButton("𝗟𝗜𝗩𝗘 𝗖𝗖", url="https://t.me/VrindavanNEeko16008"),
-
-],
+             
 [
-              InlineKeyboardButton("𝗕𝗔𝗡 𝗔𝗟𝗟︎", url=f"https://github.com/AvyuktX/AvyuktXBANALL"),
-              InlineKeyboardButton("︎𝗠𝗨𝗦𝗜𝗖", url=f"https://github.com/AvyuktX/AvyuktXMUSIC"),
-              ],
-              [
-              InlineKeyboardButton("𝗠𝗔𝗡𝗔𝗚𝗘𝗠𝗘𝗡𝗧︎", url=f"https://github.com/AvyuktX/YumikooRobot"),
-InlineKeyboardButton("𝗖𝗛𝗔𝗧 𝗕𝗢𝗧", url=f"https://github.com/AvyuktX/AvyuktXCHATBOT"),
-],
-[
-InlineKeyboardButton("𝗦𝗧𝗥𝗜𝗡𝗚𝗕𝗢𝗧", url=f"https://github.com/AvyuktX/AvyuktXSTRINGBOT"),
-InlineKeyboardButton("𝗖𝗛𝗔𝗧𝗚𝗣𝗧", url=f"https://github.com/AvyuktX/AvyuktXCHATGPT"),
-],
-[
-              InlineKeyboardButton("𝗩𝗣𝗦", url=f"https://github.com/AvyuktX/Kaali-Linux"),
-              InlineKeyboardButton("𝗠𝗢𝗩𝗜𝗘︎", url=f"https://github.com/AvyuktX/AvyuktXMOVIEBOT"),
-              ],
-              [
-              InlineKeyboardButton("𝗦𝗧𝗥𝗜𝗡𝗚 𝗛𝗔𝗖𝗞︎", url=f"https://github.com/AvyuktX/AvyuktXSTRINGHACK"),
-InlineKeyboardButton("𝗜𝗗 𝗖𝗛𝗔𝗧 𝗕𝗢𝗧", url=f"https://github.com/AvyuktX/AvyuktXIDCHAT"),
-],
-[
-InlineKeyboardButton("𝗨𝗦𝗘𝗥𝗕𝗢𝗧", url=f"https://github.com/AvyuktX/AvyuktXUSERBOT"),
-InlineKeyboardButton("𝗦𝗘𝗔𝗥𝗖𝗛𝗕𝗢𝗧", url=f"https://github.com/AvyuktX/SEARCH_BOT"),
-],
-[
-InlineKeyboardButton("𝗖𝗖 𝗕𝗢𝗧", url=f"https://github.com/AvyuktX/CC_BOT"),
+InlineKeyboardButton("𝖧𝗎𝗋𝗍𝖾𝖽", url=f"https://github.com/AnnayanX/Hurted"),
 
         ]]
     
