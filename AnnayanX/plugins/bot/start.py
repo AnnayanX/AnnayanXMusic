0ecdf1b017c7733a759d77c2cb3015f45b74f485
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-02-09 09:11:33 +0530

    Update start.py

--- AnnayanX/plugins/bot/start.py
+++ AnnayanX/plugins/bot/start.py
@@ -28,18 +28,18 @@ from strings import get_string
 
 
 YUMI_PICS = [
-"https://telegra.ph/file/2e85d11aefdf6cd01301b.jpg",
-"https://telegra.ph/file/0a08b180583f13952336a.jpg",
-"https://telegra.ph/file/ace92d59d19127d2d4e89.jpg",
-"https://telegra.ph/file/bb0a28259990c6a978985.jpg",
-"https://telegra.ph/file/ace92d59d19127d2d4e89.jpg",
-"https://telegra.ph/file/a0db46dfacd94e489117b.jpg",
-"https://telegra.ph/file/cd77be2595cdc2fca60a3.jpg",
-"https://telegra.ph/file/632724b3d30c691247c77.jpg",
-"https://telegra.ph/file/a2d01afe4f2cb1d4b650c.jpg",
-"https://telegra.ph/file/94dc035df11dfb159b999.jpg",
-"https://telegra.ph/file/fed9a5b1cbaaefc3a818c.jpg",
-"https://telegra.ph/file/66fd03632cbb38bdb4193.jpg"
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg",
+"https://graph.org/file/26f38953a92a99f99fa1d.jpg"
 
 ]
 
