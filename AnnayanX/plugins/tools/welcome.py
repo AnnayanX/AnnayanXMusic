0b53658bfc6aca2af4303080a0dac6105423d2fb
AnnayanX <148120781+annayanxd@users.noreply.github.com>
2024-02-22 02:59:10 +0530

    Update welcome.py

--- AnnayanX/plugins/tools/welcome.py
+++ AnnayanX/plugins/tools/welcome.py
@@ -90,7 +90,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
   #  draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
 
     #
-    pfp_position = (48, 88)
+    pfp_position = (52, 88)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
