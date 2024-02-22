1342f937c7484e4fcc0172a1b9d44f1a7493a1fc
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-02-23 03:08:35 +0530

    Update whisper.py

--- AnnayanX/plugins/tools/whisper.py
+++ AnnayanX/plugins/tools/whisper.py
@@ -110,8 +110,8 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="💒 Whisper",
-            description=f"@YumikooBot [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@YumikooBot (Target Username or ID) (Your Message).\n\n**Example:**\n@YumikooBot @username I Wanna Phuck You"),
+            description=f"@AnnayanXMusicBot [USERNAME | ID] [TEXT]",
+            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@AnnayanXMusicBot (Target Username or ID) (Your Message).\n\n**Example:**\n@AnnayanXMusicBot @username AnnayanX"),
             thumb_url="https://te.legra.ph/file/3eec679156a393c6a1053.jpg",
             reply_markup=switch_btn
         )
