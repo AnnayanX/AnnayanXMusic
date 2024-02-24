e7ef5bcbfcaa62e3bf56e0e68df57cbde07a6fb4
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2024-02-24 23:56:10 +0530

    Update welcome.py

--- AnnayanX/plugins/tools/welcome.py
+++ AnnayanX/plugins/tools/welcome.py
@@ -81,14 +81,14 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("AnnayanX/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((575, 575))
+    pfp = pfp.resize((825, 824))
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('AnnayanX/assets/font.ttf', size=70)
     welcome_font = ImageFont.truetype('AnnayanX/assets/font.ttf', size=61)
     #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
     #
  #   draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
-    draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((2100, 1420), f'ID: {id}', fill=(255, 255, 255), font=font)
 #    draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
   #  draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
 
