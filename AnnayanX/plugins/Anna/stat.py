e7b66b1e3284b38281baf3df9bfdfad225d427d1
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-02-24 20:16:25 +0530

    Create stat.py

---
+++ AnnayanX/plugins/Anna/stat.py
@@ -0,0 +1,12 @@
+from pyrogram import filters, Client
+from AnnayanX import app
+from AnnayanX.utils.database import add_served_chat
+
+# ------------------------------------------ #
+
+@app.on_message(filters.command(["hi", "hii", "hello", "hui", "good", "gm", "ok", "bye", "welcome", "thanks"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group)
+async def bot_check(_, message):
+    chat_id = message.chat.id
+    await add_served_chat(chat_id)
+
+# ------------------------------------------ #
