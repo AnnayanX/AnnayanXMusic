f2bd9850cf8e77f2f412d649ab282e65359a5cbe
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-03-19 15:54:26 +0530

    Update banall.py

--- AnnayanX/plugins/admins/banall.py
+++ AnnayanX/plugins/admins/banall.py
@@ -1,26 +1,24 @@
 from AnnayanX import app
-from config import BOT_ID, OWNER_ID
-from pyrogram import filters
-from pyrogram.types import ChatPermissions
+from config import OWNER_ID
+from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from AnnayanX.utils.daxx_ban import admin_filter
+from AnnayanX.misc import SUDOERS
 
-# SUDOERS list should be defined in AnnayanX.misc or elsewhere
-SUDOERS = [7099470799]  # Example list of sudo users
+# Assign a valid BOT_ID here
+BOT_ID = "6456164805"
 
-@app.on_message(filters.command("banall") & filters.user(SUDOERS))
+@app.on_message(filters.command("banall") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id    
     bot = await app.get_chat_member(chat_id, BOT_ID)
-    bot_permission = bot.can_restrict_members
-    
+    bot_permission = bot.privileges.can_restrict_members == True    
     if bot_permission:
-        async for member in app.iter_chat_members(chat_id):
+        async for member in app.get_chat_members(chat_id):       
             try:
-                if member.user.is_bot:
-                    continue  # Skip banning bots
-                await app.kick_chat_member(chat_id, member.user.id)
-                await msg.reply_text(f"‣ One more user banned.\n\n➻ {member.user.mention if member.user.username else 'User ID: ' + str(member.user.id)}")
-            except Exception as e:
-                print(f"Error banning user: {e}")
-                continue
+                await app.ban_chat_member(chat_id, member.user.id)
+                await msg.reply_text(f"‣ ᴏɴᴇ ᴍᴏʀᴇ ʙᴀɴɴᴇᴅ.\n\n➻ {member.user.mention}")                    
+            except Exception:
+                pass
     else:
-        await msg.reply_text("Either I don't have the right to restrict users or you are not a sudo user.")
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
