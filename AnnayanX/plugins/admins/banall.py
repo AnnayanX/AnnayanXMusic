ff9820379b98458eb1fd5650545357f947e062af
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-03-19 18:23:55 +0530

    Update banall.py

--- AnnayanX/plugins/admins/banall.py
+++ AnnayanX/plugins/admins/banall.py
@@ -1,24 +1,33 @@
 from AnnayanX import app
 from config import OWNER_ID
+from config import BOT_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from AnnayanX.utils.daxx_ban import admin_filter
-from AnnayanX.misc import SUDOERS
+from AnnayanX.utils.annayanx_ban import admin_filter
+BOT_ID="6456164805"
 
-# Assign a valid BOT_ID here
-BOT_ID = "6456164805"
 
-@app.on_message(filters.command("banall") & SUDOERS)
+@app.on_message(filters.command("unbanall") & admin_filter)
 async def ban_all(_, msg):
-    chat_id = msg.chat.id    
+    chat_id = msg.chat.id
+    x = 0
     bot = await app.get_chat_member(chat_id, BOT_ID)
-    bot_permission = bot.privileges.can_restrict_members == True    
+    bot_permission = bot.privileges.can_restrict_members == True
     if bot_permission:
-        async for member in app.get_chat_members(chat_id):       
+        banned_users = []
+        async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BANNED):
+            banned_users.append(m.user.id)
             try:
-                await app.ban_chat_member(chat_id, member.user.id)
-                await msg.reply_text(f"‣ ᴏɴᴇ ᴍᴏʀᴇ ʙᴀɴɴᴇᴅ.\n\n➻ {member.user.mention}")                    
+                await app.ban_chat_member(chat_id, banned_users[x])
+                print(f"ʙᴀɴɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {m.user.mention}")
+                x += 1
             except Exception:
                 pass
     else:
         await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
+
+@app.on_callback_query(filters.regex("^stop$"))
+async def stop_callback(_, query):
+    await query.message.delete()
+
+###
